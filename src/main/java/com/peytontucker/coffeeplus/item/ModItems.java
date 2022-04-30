34412247e053127a079433abc348958afe241c06
peytontucker <peytonmtucker@gmail.com>
2022-04-29 23:38:59 -0400

    initial commit

---
+++ src/main/java/com/peytontucker/coffeeplus/item/ModItems.java
@@ -0,0 +1,7 @@
+package com.peytontucker.coffeeplus.item;
+
+public class ModItems {
+
+    public static final DefferedRegister<Item> ITEMS =
+            DefferedRegister.create(ForgeRegistries.ITEMS, CoffeeMod.MOD_ID);
+}
