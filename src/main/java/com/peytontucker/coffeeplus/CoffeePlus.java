f1b2846539cf9da3f0b2eea5102f5d7b1a83cddc
Cole <dcdavis.bio@gmail.com>
2022-08-04 16:26:31 -0400

    Added coffee grinder block. Needs texture.

--- src/main/java/com/peytontucker/coffeeplus/CoffeePlus.java
+++ src/main/java/com/peytontucker/coffeeplus/CoffeePlus.java
@@ -1,5 +1,6 @@
 package com.peytontucker.coffeeplus;
 
+import com.peytontucker.coffeeplus.block.ModBlocks;
 import com.peytontucker.coffeeplus.item.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -34,6 +35,7 @@ public class CoffeePlus
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
         // Register the enqueueIMC method for modloading
--- src/main/java/com/peytontucker/coffeeplus/block/ModBlocks.java
+++ src/main/java/com/peytontucker/coffeeplus/block/ModBlocks.java
@@ -1,2 +1,50 @@
-package com.peytontucker.coffeeplus.block;public class ModBlocks {
+package com.peytontucker.coffeeplus.block;
+
+import com.peytontucker.coffeeplus.CoffeePlus;
+import com.peytontucker.coffeeplus.item.ModItemGroup;
+import com.peytontucker.coffeeplus.item.ModItems;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.lwjgl.system.CallbackI;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+
+    public static final DeferredRegister<Block> BLOCKS
+            = DeferredRegister.create(ForgeRegistries.BLOCKS, CoffeePlus.MOD_ID);
+
+    // actual code for compressed coffee
+    public static final RegistryObject<Block> COFFEE_GRINDER = registerBlock("coffee_grinder",
+            () -> new Block(AbstractBlock.Properties.create(Material.ROCK)
+                    .harvestLevel(1)
+                    .harvestTool(ToolType.PICKAXE)
+                    .setRequiresTool()
+                    .hardnessAndResistance(5f)));
+
+
+    // Registers a block. This is the method that should be called to register a block.
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    // Register's a block item
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().group(ModItemGroup.COFFEEPLUS_GROUP)));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
 }
